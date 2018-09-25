6cea168c4e131b572b4e180a3bfdd7e10d634e46
Jrew <jrewmohamed@gmail.com>
2018-09-25 16:59:45 -0400

    Added style and fixed bug that didnt allow new gifs to be paused after a new button was added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,61 @@
 let apiKey = "RQS1OIvsf8cZjSyPeVF8O8zdhrblP7i6";
-let topics = ["trucks", "motorcycles", "planes"];
+let topics = ["tesla roadster", "mclaren p1", "audi r8", "lamborghini aventador", "ferrari laferrari", "koenigsegg agera", "hennessey venom"];
 
 for (let i = 0; i < topics.length; i++) {
-    $("#buttonHolder").append(`<button id="${topics[i]}">${topics[i]}</button>`);
+  $("#buttonHolder").append(`<button id="${topics[i]}">${topics[i]}</button>`);
 }
 
-$("#userSubmit").click(function () {
-    event.preventDefault();
-    if ($("#userInput").val() !== "") {
-        let newWord = $("#userInput").val().trim();
-        topics.push(newWord);
-        $("#buttonHolder").append(`<button id="${newWord}">${newWord}</button>`);
-    };
-    console.log(topics);
+$("#userSubmit").click(function (event) {
+
+  event.preventDefault();
+  if ($("#userInput").val() !== "") {
+    let newWord = $("#userInput").val().trim();
+    topics.push(newWord);
+    $("#buttonHolder").append(`<button id="${newWord}">${newWord}</button>`);
+  };
+  $("#userInput").val("");
+
 });
 
-$(document).on("click","button",function () {
+$(document).on("click", "button", function () {
 
-    let chosenWord = $(this).attr(`id`);
+  $("#gifHolder").empty();
 
-    console.log(chosenWord);
+  let chosenWord = $(this).attr(`id`);
 
-    let gif = `https://api.giphy.com/v1/gifs/search?q=${chosenWord}&api_key=${apiKey}&limit=10`;
+  let gif = `https://api.giphy.com/v1/gifs/search?q=${chosenWord}&api_key=${apiKey}&limit=10`;
 
-    $.ajax({
-        url: gif,
-        method: "GET",
-    }).then(function (response) {
+  $.ajax({
+    url: gif,
+    method: "GET",
+  }).then(function (response) {
 
-        console.log(response);
+    console.log(response);
 
-        for (let i = 0; i < response.data.length; i++) {
-            
-            $("#gifHolder").append(`<p>Rating: ${response.data[i].rating}</p>`)
-            $("#gifHolder").append(`<img id="giphy${[i]}">`);
-            $(`#giphy${[i]}`).attr("src", `${response.data[i].images.fixed_height_small.url}`)
-        };
+    for (let i = 0; i < response.data.length; i++) {
 
-    });
+      $("#gifHolder").append(`<div class="pandgifHolder" id="pandgif${[i]}"></div>`)
+      $(`#pandgif${[i]}`).append(`<p class="rtg">Rating: ${response.data[i].rating}</p>`)
+      $(`#pandgif${[i]}`).append(`<img id="giphy${i}" value=${i}>`);
+      $(`#giphy${i}`).attr("src", `${response.data[i].images.fixed_height_small_still.url}`)
+      $(`#giphy${i}`).attr("state", `still`)
+
+    };
+
+    //Gave me the world of trouble because I originally had $(document).on instead.
+    $("img").on("click", function () {
+
+      numVal = $(this).attr('value');
+      if ($(this).attr('state') === 'still') {
+        $(this).attr(`src`, `${response.data[numVal].images.fixed_height_small.url}`);
+        $(this).attr(`state`, `not-still`);
+      } else if ($(this).attr('state') === 'not-still') {
+        $(this).attr(`src`, `${response.data[numVal].images.fixed_height_small_still.url}`)
+        $(this).attr(`state`, `still`);
+      };
+
+    })
+
+  });
 
 });
---
+++ assets/pseudocode.md
@@ -0,0 +1,26 @@
+#Fix bug that doesn't allow new gifs to play when pressed.
+#If the user clicks a new button and the clicks the gif, the old gif plays for a split second and then is updated to the new gif.
+
+#Fix bug that takes away old gifs after a new button was pressed and appended
+
+#Fix bug that keeps reseting the gif with the same gif regardless of the gif's index
+
+#Add style
+
+
+Pseudo:
+
+#Make a list of buttons on top of the screen
+
+#Make a search bar on the right side of screen
+#  when a user clicks the submit button after typing in the input
+#    a new button is added to the top of the screen
+
+#When a user clicks a button
+#  10 still gifs populate
+
+#If a user clicks the still gif
+#  the gif starts playing
+
+#If a user clicks gif while its playing
+#  the gif stops
